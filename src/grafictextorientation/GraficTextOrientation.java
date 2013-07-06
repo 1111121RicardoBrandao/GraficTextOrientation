e12473add619f1ae434de2e7d8949132378f149c
Ricardo Brandao <ricardombrandao@gmail.com>
2013-07-06 17:31:46 +0100

    Start of project

---
+++ src/grafictextorientation/GraficTextOrientation.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grafictextorientation;
+
+/**
+ *
+ * @author Scare
+ */
+public class GraficTextOrientation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Grafic test = new Grafic("Exemplo",Grafic.orientationOptions.Horizontal);
+    }
+}
